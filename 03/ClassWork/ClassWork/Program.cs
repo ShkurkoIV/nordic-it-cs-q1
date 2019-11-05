377e8a07ccb7ece3b83b3ff1e83eac102b949f78
Direaper <jimac221@gmail.com>
2019-11-05 21:54:30 +0300

    класная работа

---
+++ 03/ClassWork/ClassWork/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassWork
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = new int[5];
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				//Console.WriteLine(array[i]);
+			}
+			foreach (var item in array)
+			{
+				Console.WriteLine(item);
+			}
+			Console.ReadKey();
+		}
+
+		
+	
+	}
+}

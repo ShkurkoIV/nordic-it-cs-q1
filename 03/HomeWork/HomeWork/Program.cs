297d90a3e247ec971125734bcf6122328e0ed96f
Ivan Shkurko <ivanshkurko@gmail.com>
2019-11-06 15:48:17 +0300

    дз 3 конец

--- 03/HomeWork/HomeWork/Program.cs
+++ 03/HomeWork/HomeWork/Program.cs
@@ -6,7 +6,32 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
-            
+            uint[,] tab;
+            uint columns;
+            uint rows;
+            Console.WriteLine("Введите число колонок");
+            uint.TryParse(Console.ReadLine(), out columns);
+            Console.WriteLine("Введите число строк");
+            uint.TryParse(Console.ReadLine(), out rows);
+
+            Console.WriteLine($"Колонок: {columns}");
+            Console.WriteLine($"Строк: {rows}");
+
+            tab = new uint[columns, rows];
+
+            for (uint i = 0; i < columns; i++)
+            {
+                for (uint j = 0; j < rows; j++)
+                {
+                    tab[i, j] = (i + 1) * (j + 1);
+                    if (j == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                    Console.Write(tab[i, j] + " ");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }

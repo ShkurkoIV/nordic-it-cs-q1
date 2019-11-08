6a556118db9a8d356e1c79e847ca58b068709e87
Direaper <jimac221@gmail.com>
2019-11-08 19:15:48 +0300

    занятие 4

--- 04/ClassWork/ClassWork/Program.cs
+++ 04/ClassWork/ClassWork/Program.cs
@@ -6,7 +6,32 @@ namespace ClassWork
 	{
 		static void Main(string[] args)
 		{
-			
+			int a = 100;
+			int b = 17;
+
+			Console.WriteLine($"a + b = {a + b}");
+			Console.WriteLine($"a - b = {a - b}");
+			Console.WriteLine($"a * b = {a * b}");
+			Console.WriteLine($"a / b = {a / b}");
+			Console.WriteLine($"a % b = {a % b}");
+
+			Console.WriteLine();
+			double c = 48.13;
+			float d = 2.5F;
+
+			Console.WriteLine($"c + d = {c + d}");
+			Console.WriteLine($"c - d = {c - d}");
+			Console.WriteLine($"c * d = {c * d}");
+			Console.WriteLine($"c / d = {c / d}");
+			Console.WriteLine($"c % d = {c % d}");
+
+			for (int i = 0; i < 10; ++i)
+			{
+				Console.WriteLine(i);
+			}
+
+			Console.ReadLine();
+
 		}
 	}
 }

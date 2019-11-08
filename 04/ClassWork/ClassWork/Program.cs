8c035f5e894e847bb48c9e3536e25622c190217b
Direaper <jimac221@gmail.com>
2019-11-08 21:05:34 +0300

    4.1

--- 04/ClassWork/ClassWork/Program.cs
+++ 04/ClassWork/ClassWork/Program.cs
@@ -6,6 +6,7 @@ namespace ClassWork
 	{
 		static void Main(string[] args)
 		{
+			/*
 			int a = 100;
 			int b = 17;
 
@@ -29,8 +30,38 @@ namespace ClassWork
 			{
 				Console.WriteLine(i);
 			}
+			
+			*/
+
+			/*
+			int a = 18;
+			int b = a++;
+			Console.WriteLine(a == b);
+			Console.WriteLine(a != b);
+			Console.WriteLine(a > b);
+			Console.WriteLine(a < b);
+			Console.WriteLine(a >= b);
+			Console.WriteLine(a <= b);
+			*/
+
+			int a = int.Parse(Console.ReadLine());
+			double h = double.Parse(Console.ReadLine());
+			int n = int.Parse(Console.ReadLine());
+
+			var x = a / (2 * Math.Tan((180 / n) / 57.3));
+			var y = (n * a) / 2;
+
+			var sFull = y * (x + Math.Sqrt(Math.Pow(h, 2) + Math.Pow(x, 2)));
+			var s = y * (Math.Sqrt(Math.Pow(h, 2) + Math.Pow(x, 2)));
+			var v = (h * n * Math.Pow(a, 2)) / (12 * Math.Tan((180 / n) / 57.3));
+
+			Console.WriteLine(sFull);
+			Console.WriteLine(s);
+			Console.WriteLine(v);
+
 
 			Console.ReadLine();
+
 
 		}
 	}

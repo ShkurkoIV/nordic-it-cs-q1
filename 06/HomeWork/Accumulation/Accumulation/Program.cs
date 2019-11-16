00665bf3281ac18164d3ad57d411c00a6f39142e
Ivan Shkurko <ivanshkurko@gmail.com>
2019-11-16 22:04:19 +0300

    домашка 3

---
+++ 06/HomeWork/Accumulation/Accumulation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        delegate bool IsEven(int i);
+        static void Main(string[] args)
+        {
+        Start:
+            double contribution;
+            double income;
+            double accumulation;
+            try
+            {
+                Console.WriteLine("Введите сумму первоначального взноса в рублях");
+                contribution = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите ежедневный процент дохода в виде десятичной дроби");
+                income = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите желаемую сумму накопления в рублях");
+                accumulation = Double.Parse(Console.ReadLine());
+                if (contribution <= 0 || income <= 0 || accumulation <= 0 || (accumulation < contribution))
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка ввода. Попробуйте еще раз \n");
+                goto Start;
+            }
+            double amountPerDay = contribution;
+            int days;
+            for (days = 0; amountPerDay < accumulation; days++)
+            {
+                amountPerDay += amountPerDay * income;
+            }
+            Console.WriteLine($"Необходимое количество дней для накопления желаемой суммы: {days}.");
+        }
+    }
+}
---
+++ 06/HomeWork/EvenNumbers/EvenNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        delegate bool IsEven(int i);
+        static void Main(string[] args)
+        {
+        Start:
+            Console.WriteLine("Введите натуральное число");
+            bool error;
+            uint number;
+            List<int> result = EvenNumbers(Console.ReadLine(), i => i % 2 == 0 & i != 0, out error, out number);
+            if (error)
+            {
+                Console.WriteLine("Введено неверное значение! Попробуйте ещё раз: \n");
+                goto Start;
+            }
+            Console.WriteLine($"В числе {number} содержится следующее количество четных цифр: {result.Count}");
+            Console.ReadKey();
+        }
+        private static List<int> EvenNumbers(string input, IsEven func, out bool error, out uint number)
+        {
+            List<int> result = new List<int>();
+            error = !uint.TryParse(input, out number) || number == 0;
+            if (error)
+            {
+                return result;
+            }
+            foreach (var item in input.ToCharArray())
+            {
+                var i = (int)Char.GetNumericValue(item);
+                if (func(i))
+                {
+                    result.Add(i);
+                }
+            }
+            return result;
+        }
+    }
+}
